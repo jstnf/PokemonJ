123f54cd5192bad9b5ae5332923db3ed88cd4ec4
CURTIS LIU <705470@district.pylusd.k12.ca.us>
2019-06-03 11:31:16 -0700

    Package

---
+++ src/io/pokemonj/curtis/Move.java
@@ -0,0 +1,5 @@
+package io.pokemonj.curtis;
+
+public class Move {
+
+}
---
+++ src/io/pokemonj/curtis/Pokemon.java
@@ -0,0 +1,9 @@
+package io.pokemonj.curtis;
+
+public class Pokemon {
+	//sprites?
+	public Pokemon(String name, String type1, String type2, ArrayList<Move> moveset, int[] baseStats)
+	{
+		
+	}
+}
